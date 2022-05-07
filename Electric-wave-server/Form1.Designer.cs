40bf098bc600c093efddeb374935c3369a35848d
HbiZHI <tungsyulou@gmail.com>
2022-05-07 20:38:44 +0800

    上传了电波的1.0版服务端软件

---
+++ Electric-wave-server/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace _62Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListening = new System.Windows.Forms.Button();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtFileaddress = new System.Windows.Forms.TextBox();
+            this.btnChoose = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnShake = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.cboIP = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnListening
+            // 
+            this.btnListening.Location = new System.Drawing.Point(382, 72);
+            this.btnListening.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnListening.Name = "btnListening";
+            this.btnListening.Size = new System.Drawing.Size(100, 31);
+            this.btnListening.TabIndex = 0;
+            this.btnListening.Text = "开始监听";
+            this.btnListening.UseVisualStyleBackColor = true;
+            this.btnListening.Click += new System.EventHandler(this.btnListening_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(44, 337);
+            this.txtMsg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(651, 169);
+            this.txtMsg.TabIndex = 10;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(44, 74);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(197, 27);
+            this.txtAddress.TabIndex = 3;
+            this.txtAddress.Text = "192.168.31.181";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(261, 72);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(102, 27);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "60000";
+            this.txtPort.TextChanged += new System.EventHandler(this.txtPort_TextChanged);
+            // 
+            // txtFileaddress
+            // 
+            this.txtFileaddress.Location = new System.Drawing.Point(44, 528);
+            this.txtFileaddress.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFileaddress.Name = "txtFileaddress";
+            this.txtFileaddress.Size = new System.Drawing.Size(470, 27);
+            this.txtFileaddress.TabIndex = 5;
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(533, 528);
+            this.btnChoose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(71, 27);
+            this.btnChoose.TabIndex = 6;
+            this.btnChoose.Text = "选择";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(617, 528);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(78, 27);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "发送文件";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(717, 337);
+            this.btnEnter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(89, 71);
+            this.btnEnter.TabIndex = 8;
+            this.btnEnter.Text = "发送";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnShake
+            // 
+            this.btnShake.Location = new System.Drawing.Point(717, 435);
+            this.btnShake.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnShake.Name = "btnShake";
+            this.btnShake.Size = new System.Drawing.Size(89, 70);
+            this.btnShake.TabIndex = 9;
+            this.btnShake.Text = "震动";
+            this.btnShake.UseVisualStyleBackColor = true;
+            this.btnShake.Click += new System.EventHandler(this.btnShake_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(44, 135);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(651, 169);
+            this.txtLog.TabIndex = 11;
+            // 
+            // cboIP
+            // 
+            this.cboIP.FormattingEnabled = true;
+            this.cboIP.Location = new System.Drawing.Point(499, 75);
+            this.cboIP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboIP.Name = "cboIP";
+            this.cboIP.Size = new System.Drawing.Size(196, 28);
+            this.cboIP.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 607);
+            this.Controls.Add(this.cboIP);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnShake);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.txtFileaddress);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnListening);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Electric wave服务端v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListening;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtFileaddress;
+        private System.Windows.Forms.Button btnChoose;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnShake;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.ComboBox cboIP;
+    }
+}
+
---
+++ Electric-wave-server/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _62Server
+{
+    public partial class Form1 : Form
+    {
+        Socket socketSend;
+        //将远程连接的客户端的IP地址和Socket存入集合
+        Dictionary<string, Socket> dicIP = new Dictionary<string, Socket>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 开始监听
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnListening_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //当点击开始监听的时候 在服务器端创建一个负责监听IP地址和端口号的Socket
+                Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Any;
+                //创建端口号对象
+                IPEndPoint port = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+                //监听
+                socketWatch.Bind(port);
+                ShowMsg("监听成功");
+                //创建监听队列
+                socketWatch.Listen(10);
+                //创建监听线程
+                Thread th = new Thread(Listen);
+                th.IsBackground = true;
+                th.Start(socketWatch);
+            }
+            catch { }
+        }
+
+
+        /// <summary>
+        /// 等待客户端的连接 并且创建与之通信的Socket
+        /// </summary>
+        /// <param name="o"></param>
+        void Listen(object o)
+        {
+            try
+            {
+                //用as进行类型转换，如果能转换返回一个对象，否则返回一个null
+                Socket socketWatch = o as Socket;
+                //等待客户端连接 并且创建一个负责通信的Socket
+                while (true)
+                {
+                    socketSend = socketWatch.Accept();
+                    //将远程连接的客户端的IP地址和Socket存入集合
+                    dicIP.Add(socketSend.RemoteEndPoint.ToString(), socketSend);
+                    //将远程连接的客户端的IP地址和Socket存入下拉框
+                    cboIP.Items.Add(socketSend.RemoteEndPoint.ToString());
+                    //有人连接成功后写入日志
+                    ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+                    //开启一个新线程接收客户端发送的消息
+                    Thread th = new Thread(Receive);
+                    th.IsBackground = true;
+                    th.Start(socketSend);
+                }
+            }
+            catch
+            { }
+        }
+
+        void Receive(object o)
+        {
+            Socket socketSend = o as Socket;
+            while(true)
+            {
+                try
+                {
+                    //客户端连接成功后，服务器接收客户端发来的消息
+                    byte[] buffer = new byte[1024 * 1024 * 2];
+                    //实际上接收到的有效字节数
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    string str = Encoding.UTF8.GetString(buffer, 0, r);
+                    ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                }
+                catch
+                { }
+            }
+        }
+
+        /// <summary>
+        /// 显示消息
+        /// </summary>
+        /// <param name="str"></param>
+        private void ShowMsg(string str)
+        {
+            txtLog.AppendText(str + "\r\n");
+        }
+
+        /// <summary>
+        /// 主窗体加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 服务器给客户端发消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = txtMsg.Text;
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+                //进行协议的标识
+                List<byte> list = new List<byte>();
+                list.Add(0);
+                list.AddRange(buffer);
+                //将泛型集合转换为数组
+                byte[] newBuffer = list.ToArray();
+                //发送的合法性判断
+                if (cboIP.SelectedItem == null)
+                {
+                    MessageBox.Show("请选择要发送的用户！");
+                }
+                else
+                {
+                    //获得用户在下拉框中选中的IP地址
+                    string ip = cboIP.SelectedItem.ToString();
+                    dicIP[ip].Send(newBuffer);
+                    txtMsg.Clear();
+                }
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// 选择要发送的文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = @"C:\Users\admin\source\repos\62Server";
+                ofd.Title = "请选择要发送的文件";
+                ofd.Filter = "所有文件|*.*";
+                ofd.ShowDialog();
+                txtFileaddress.Text = ofd.FileName;
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// 发送文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /// 获得要发送文件的路径
+                string path = txtFileaddress.Text;
+                using (FileStream fsRead = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    //创建字节数组传递数据
+                    byte[] buffer = new byte[1024 * 1024 * 5];
+                    int r = fsRead.Read(buffer, 0, buffer.Length);
+                    //添加协议位
+                    List<byte> list = new List<byte>();
+                    list.Add(1);
+                    list.AddRange(buffer);
+                    byte[] newBuffer = list.ToArray();
+                    //发送的合法性判断
+                    if (cboIP.SelectedItem == null)
+                    {
+                        MessageBox.Show("请选择要发送的用户！");
+                    }
+                    else
+                    {
+                        dicIP[cboIP.SelectedItem.ToString()].Send(newBuffer, 0, r + 1, SocketFlags.None);
+                        txtAddress.Clear();
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private void btnShake_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] buffer = new byte[1];
+                buffer[0] = 2;
+                //发送的合法性判断
+                if (cboIP.SelectedItem == null)
+                {
+                    MessageBox.Show("请选择要发送的用户！");
+                }
+                else
+                {
+                    dicIP[cboIP.SelectedItem.ToString()].Send(buffer, 0, buffer.Length, SocketFlags.None);
+                    txtAddress.Clear();
+                }
+            }
+            catch { }
+        }
+
+        private void txtPort_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
