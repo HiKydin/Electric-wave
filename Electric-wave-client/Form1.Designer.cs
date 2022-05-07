d18988b184022edd2bcebefd98b7dfd828a23042
HbiZHI <tungsyulou@gmail.com>
2022-05-07 21:31:06 +0800

    上传了电波的1.0.0版客户端软件

---
+++ Electric-wave-client/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace _63Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(55, 100);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(700, 170);
+            this.txtLog.TabIndex = 21;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(665, 300);
+            this.btnEnter.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(90, 70);
+            this.btnEnter.TabIndex = 18;
+            this.btnEnter.Text = "发送";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(270, 50);
+            this.txtPort.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(160, 27);
+            this.txtPort.TabIndex = 14;
+            this.txtPort.Text = "请输入服务器端口号";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(55, 50);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(200, 27);
+            this.txtAddress.TabIndex = 13;
+            this.txtAddress.Text = "请输入服务器IP地址";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(55, 300);
+            this.txtMsg.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(589, 169);
+            this.txtMsg.TabIndex = 20;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(470, 50);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(70, 27);
+            this.btnConnect.TabIndex = 12;
+            this.btnConnect.Text = "连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(665, 399);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 70);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "震动";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 553);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btnConnect);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Electric wave 客户端v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Electric-wave-client/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _63Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //创建负责通信的Socket
+        Socket socketSend = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //获取要连接的服务器的IP地址和端口号
+                IPAddress ip = IPAddress.Parse(txtAddress.Text);
+                IPEndPoint port = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+                socketSend.Connect(port);
+                ShowMsg("连接成功!");
+                //创建一个新线程不断接收服务器发送的消息
+                Thread th = new Thread(Receive);
+                th.IsBackground = true;
+                th.Start(socketSend);
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// 客户端接收消息
+        /// </summary>
+        /// <param name="o"></param>
+        void Receive(object o)
+        {
+            Socket socketSend = o as Socket;
+            while(true)
+            {
+                try
+                {
+                    //连接成功后，接收服务器发来的消息
+                    byte[] buffer = new byte[1024 * 1024 * 2];
+                    //实际上接收到的有效字节数
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    //提取出接收内容的协议
+                    //==0表示传输的是文字消息
+                    if (buffer[0]==0)
+                    {
+                        string str = System.Text.Encoding.UTF8.GetString(buffer, 1, r-1);
+                        ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                    }
+                    //==1表示传输的是文件
+                    else if(buffer[0] ==1)
+                    {
+                        MessageBox.Show("对方发送了一个文件，请查收！");
+                        SaveFileDialog sfd = new SaveFileDialog();
+                        sfd.InitialDirectory = @"C:\Users\admin\source\repos\62Server";
+                        sfd.Title = "请选择要保存的路径";
+                        sfd.Filter = "所有文件|*.*";
+                        sfd.ShowDialog(this);
+                        string path = sfd.FileName;
+                        using(FileStream fsWrite=new FileStream(path,FileMode.OpenOrCreate,FileAccess.Write))
+                        {
+                            fsWrite.Write(buffer, 1, r - 1);
+                        }
+                        MessageBox.Show("保存成功！");
+                    }
+                    //==2表示传输的是震动
+                    else if(buffer[0] ==2)
+                    {
+                        Shake();
+                    }
+                }
+                catch { }
+            }
+        }
+        private void Shake()
+        {
+            int leftWidth = this.Left; //指定窗体左边值
+            int topWidth = this.Top; //指定窗体上边值
+
+            for (int i = 0; i < 20; i++) //设定循环次数为20 且加1
+            {
+                if (i % 2 == 0) //如果i 能给2整除
+                {
+                    this.Left = this.Left + 10; //窗体左边值加10
+                }
+                else //否则
+                {
+                    this.Left = this.Left - 10;//窗体左边边值减10
+                }
+                if (i % 2 == 0)//如果i能给2整除
+                {
+                    this.Top = this.Top + 10;//窗体上边值加10
+                }
+                else//否则
+                {
+                    this.Top = this.Top - 10;//窗体上边值减10
+                }
+
+                System.Threading.Thread.Sleep(30);//震动频率
+            }
+
+            this.Left = leftWidth;//重设窗体初此左边值
+            this.Top = topWidth; //重设窗体初此上边值
+        }
+
+        /// <summary>
+        /// 客户端给服务器发送消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string str = txtMsg.Text.Trim();
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+                socketSend.Send(buffer);
+                txtMsg.Clear();
+            }
+            catch { }
+        }
+
+        /// <summary>
+        /// 显示消息
+        /// </summary>
+        /// <param name="str"></param>
+        private void ShowMsg(string str)
+        {
+            txtLog.AppendText(str + "\r\n");
+        }
+
+        /// <summary>
+        /// 主窗体加载
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
